0a376d84bb862efb05d11100f5595f45f29d2037
Ademola Emmanuel <111035925+Derek2222@users.noreply.github.com>
2022-12-01 11:13:05 -0800

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'e' and chr(i) != 'q':
         print('{:c}'.format(i), end='')
->>>>>>> 4adcc029f5a730bcb20c3b959691ef70cd78a1f9
