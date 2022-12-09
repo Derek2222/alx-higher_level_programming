9ea2c3e264386416507937c59728795f9c364ba7
Derek2222 <drademola4@gmail.com>
2022-12-08 22:07:51 -0800

    fourth python more data

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,4 +6,5 @@ def complex_delete(a_dictionary, value):
             keys_to_del.append(key)
             for key in keys_to_del:
                 del a_dictionary[key]
+            return a_dictionary
         
